b5a00ac3681782961494667ea9c9de8b49bd02be
andrei-kryvetski <kryvetskistud@gmail.com>
2021-09-12 00:02:42 +0300

    feat: add post component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
+import PostItem from "./components/PostItem";
+import "./styles/App.css"
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
   return (
-   <h2>hollaa</h2>
+  <div className="App">
+    <h2>Post List</h2>
+    {posts.map(post => 
+      <PostItem post={post} key={post.id}/>
+        )}
+
+  </div>
   )
 }
 
