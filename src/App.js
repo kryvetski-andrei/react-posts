75c996ef12d1db2d5972710be3404975135ea7c7
andrei-kryvetski <kryvetskistud@gmail.com>
2021-09-12 14:46:10 +0300

    feat: add adding post feature

--- src/App.js
+++ src/App.js
@@ -5,26 +5,47 @@ import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css"
 
 function App() {
-  const [posts] = useState([
+  const [posts, setPosts] = useState([
     {id: 1, title: 'JavaScript', body: 'Description'},
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
 
-  const [posts2] = useState([
-    {id: 1, title: 'NodeJS', body: 'Description'},
-    {id: 2, title: 'NodeJS 2', body: 'Description'},
-    {id: 3, title: 'NodeJS 3', body: 'Description'}
-  ])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNewPost = (event) => {
+    event.preventDefault()
+
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+
+    setPosts([...posts, newPost])
+  }
 
   return (
     <div className="App">
-      <MyInput type="text" placeholder="Название поста"/>
-      <MyInput type="text" placeholder="Содержимое поста"/>
-      <MyButton disabled style={{backgroundColor: "#347D39", color: "#FFFFFF"}}>Создать пост</MyButton>
+      <form>
+        <MyInput 
+          type="text"
+          value={title} 
+          placeholder="Название поста"
+          onChange = {event => setTitle(event.target.value)}
+        />
+        <MyInput         
+          type="text"
+          value={body}  
+          placeholder="Содержимое поста"
+          onChange = {event => setBody(event.target.value)}
+        />
+        <MyButton onClick={addNewPost} style={{backgroundColor: "#347D39", color: "#FFFFFF"}}>Создать пост</MyButton>
+      </form>
+      
 
       <PostList posts={posts} title="Post List JS"/>
-      <PostList posts={posts2} title="Post List NodeJS"/>
     </div>
     
   )
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "../input/MyInput.module.css"
-const MyInput = (props) => {
+const MyInput = React.forwardRef((props, ref) => {
   return (
-    <input className = {classes.myInput} {...props}/>
+    <input className = {classes.myInput} ref={ref} {...props}/>
   )
-}
+})
 
 export default MyInput;
