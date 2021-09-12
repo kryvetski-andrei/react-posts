90c5a3697ba05d9379505eecabe15d105a4aeff9
andrei-kryvetski <kryvetskistud@gmail.com>
2021-09-12 13:00:56 +0300

    feat: add button component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react"
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 import "./styles/App.css"
 
 function App() {
-  const [posts, setPosts] = useState([
+  const [posts] = useState([
     {id: 1, title: 'JavaScript', body: 'Description'},
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
 
-  const [posts2, setPosts2] = useState([
+  const [posts2] = useState([
     {id: 1, title: 'NodeJS', body: 'Description'},
     {id: 2, title: 'NodeJS 2', body: 'Description'},
     {id: 3, title: 'NodeJS 3', body: 'Description'}
@@ -17,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+      <MyButton disabled>Создать пост</MyButton>
       <PostList posts={posts} title="Post List JS"/>
       <PostList posts={posts2} title="Post List NodeJS"/>
     </div>
